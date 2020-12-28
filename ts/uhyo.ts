eba5fe7b80c4e44479cdb56e7a8d8a3efc75c385
田端佑也 <tabayu5@gmail.com>
2020-12-28 12:19:32 +0900

    2-1_2-4

--- ts/uhyo.ts
+++ ts/uhyo.ts
@@ -52,3 +52,62 @@ function myFilter<T>(arr: T[], predicate: (elm: T) => boolean): T[] {
 
 const res = myFilter([1,2,3,4], num => num % 2 ===0);
 const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+
+// 2-2
+type Speed = 'slow' | 'medium' | 'fast'
+
+function getSpeed(speed: Speed): number {
+    switch (speed) {
+        case "slow":
+            return 10;
+        case "medium":
+            return 50;
+        case "fast":
+            return 200;
+    }
+}
+
+const slowSpeed = getSpeed("slow")
+console.log('slowSpeed:',slowSpeed)
+
+// 2-3
+interface AddEventListenerOptions {
+    capture?: boolean;
+    once?: boolean;
+    passive?: boolean;
+}
+
+declare function addEventListener(
+    type: string,
+    handler: () => void,
+    options?: boolean | AddEventListenerOptions
+): void;
+
+// addEventListener("foobar", () => {});
+// addEventListener("event", () => {}, true);
+
+// 2-4
+function giveId<T>(obj:T): T & {id: string} {
+    const id = "test";
+    return {
+        ...obj,
+        id
+    };
+}
+
+const obj1: {
+    id: string;
+    foo: number;
+} = giveId({ foo: 123 });
+const obj2: {
+    id: string;
+    num: number;
+    hoge: boolean;
+} = giveId({
+    num: 0,
+    hoge: true,
+})
+console.log('obj1:',obj1)
+console.log('obj2:',obj2)
+
+// 2-5
